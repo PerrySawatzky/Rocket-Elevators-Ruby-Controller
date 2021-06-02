96845835a8c82c1bef7bf36e027d9768da95749b
PerrySawatzky <sawatzky.perry@gmail.com>
2021-06-02 13:36:10 -0400

    added scenarios and refactored

--- residential_controller.rb
+++ residential_controller.rb
@@ -27,41 +27,34 @@ class Column
     def requestElevator(_requestedFloor, _direction)
         bestElevator = nil
         bestFloorGap = 10000
-
-        #cycles through elevatorsList and finds the bestFloorGap and assigns bestElevator
-        #Also ensures the best elevator is idle
+        #cycles through elevatorsList and finds the best elevator
         (0...elevatorsList.size).each do |i|
             currentFloorGap = (elevatorsList[i].currentFloor - _requestedFloor).abs
             elevatorMovingStatus = elevatorsList[i].status
-            if(elevatorMovingStatus != 'idle')
+            elevatorDirection = elevatorsList[i].direction
+            elevatorCurrentFloor = elevatorsList[i].currentFloor
+            #Elevator moving in the opposite direction, dont want it.
+            if(elevatorMovingStatus != 'idle' && _direction != elevatorDirection)
                 currentFloorGap = 10000
             end
-            if(currentFloorGap < bestFloorGap)
+            #The elevator is above the user. It is going up and so is the user.
+            if(_direction == elevatorDirection && elevatorDirection == "down" && _requestedFloor < elevatorCurrentFloor)
+                bestFloorGap = currentFloorGap
+                bestElevator = elevatorsList[i]
+            end
+            #The elevator is below the user. It is coming down and so is the user.
+            if(_direction == elevatorDirection && elevatorDirection == "up" && _requestedFloor > elevatorCurrentFloor)
+                bestFloorGap = currentFloorGap
+                bestElevator = elevatorsList[i]
+            end
+            #The elevator is idle and close to user.
+            if(currentFloorGap < bestFloorGap && elevatorMovingStatus == 'idle')
                 bestFloorGap = currentFloorGap
                 bestElevator = elevatorsList[i]
             end
         end
-        #Moves elevator up or down until it matches _requestedFloor, then opens the door
-        while(_requestedFloor < bestElevator.currentFloor)
-            bestElevator.currentFloor = bestElevator.currentFloor - 1
-            bestElevator.direction = 'down'
-            bestElevator.status = 'moving'
-            print("Elevator is on floor #{bestElevator.currentFloor}\n")
-        end
-        while(_requestedFloor > bestElevator.currentFloor)
-            bestElevator.currentFloor = bestElevator.currentFloor + 1
-            bestElevator.direction = 'up'
-            bestElevator.status = 'moving'
-            print("Elevator is on floor #{bestElevator.currentFloor}\n")
-        end
-        while(_requestedFloor == bestElevator.currentFloor)
-            bestElevator.direction = nil
-            bestElevator.door.status = 'open'
-            bestElevator.status = 'idle'
-            print("*DING* The elevator doors are #{bestElevator.door.status}\n")
-            print("Your elevator has arrived, please enter now.\n")
-            break
-        end
+        bestElevator.floorRequestList.push(_requestedFloor)
+        bestElevator.moveElevator()
         return bestElevator #So we can call on the function and give us the same elevator when using requestFloor when testing
     end
 end
@@ -99,9 +92,35 @@ class Elevator
         while self.currentFloor == _requestedFloor
             door.status = 'open'
             status = 'idle'
-            print("Your elevator has arrived, please exit now.\n")
+            print("*DING* Your elevator has arrived, please exit now.\n")
             break
         end
+    end
+    def moveElevator()
+        destination = self.floorRequestList[0]
+        while self.floorRequestList.size != 0
+            while destination > self.currentFloor
+                self.currentFloor = self.currentFloor + 1
+                self.direction = "up"
+                self.status = "moving"
+                print("Elevator is on floor #{self.currentFloor}\n")
+            end
+            while destination < self.currentFloor
+                self.currentFloor = self.currentFloor - 1
+                self.direction = "down"
+                self.status = "moving"
+                print("Elevator is on floor #{self.currentFloor}\n")
+            end
+            while destination == self.currentFloor
+                self.direction = nil
+                self.door.status = 'open'
+                print("*DING* The elevator doors are #{self.door.status}\n")
+                print("Your elevator has arrived, please enter now.\n")
+                break
+            end
+            self.floorRequestList.shift()
+        end
+        self.status = "idle"
     end
 end
 
@@ -136,10 +155,35 @@ class Door
     end
 end
 
-testColumn = Column.new(1, 10, 2)
-testColumn.elevatorsList[0].currentFloor = 2
-testColumn.elevatorsList[1].currentFloor = 6
-elevatorToUse = testColumn.requestElevator(3, 'up')
+scenario1 = Column.new(1, 10, 2)
+scenario1.elevatorsList[0].currentFloor = 2
+scenario1.elevatorsList[1].currentFloor = 6
+elevatorToUse = scenario1.requestElevator(3, 'up')
 elevatorToUse.requestFloor(7)
-print("Elevator A Scenario 1 Final Position ", testColumn.elevatorsList[0].currentFloor, "\n")
-print("Elevator B Scenario 1 Final Position ", testColumn.elevatorsList[1].currentFloor, "\n")
+print("Elevator A Scenario 1 Final Position ", scenario1.elevatorsList[0].currentFloor, "\n")
+print("Elevator B Scenario 1 Final Position ", scenario1.elevatorsList[1].currentFloor, "\n")
+
+# scenario2 = Column.new(1, 10, 2)
+# scenario2.elevatorsList[0].currentFloor = 10
+# scenario2.elevatorsList[1].currentFloor = 3
+# elevatorToUse = scenario2.requestElevator(1, 'up')
+# elevatorToUse.requestFloor(6)
+# elevatorToUse1 = scenario2.requestElevator(3, "up")
+# elevatorToUse1.requestFloor(5)
+# elevatorToUse2 = scenario2.requestElevator(9, "down")
+# elevatorToUse2.requestFloor(2)
+# print("Elevator A Scenario 2 Final Position ", scenario2.elevatorsList[0].currentFloor, "\n")
+# print("Elevator B Scenario 2 Final Position ", scenario2.elevatorsList[1].currentFloor, "\n")
+
+# scenario3 = Column.new(1, 10, 2)
+# scenario3.elevatorsList[0].currentFloor = 10
+# scenario3.elevatorsList[1].currentFloor = 3
+# scenario3.elevatorsList[1].status = "moving"
+# elevatorToUse = scenario3.requestElevator(3, "down")
+# elevatorToUse.requestFloor(2)
+# scenario3.elevatorsList[1].currentFloor = 6
+# scenario3.elevatorsList[1].status = "idle"
+# elevatorToUse1 = scenario3.requestElevator(10, "down")
+# elevatorToUse1.requestFloor(3)
+# print("Elevator A Scenario 3 Final Position ", scenario3.elevatorsList[0].currentFloor, "\n")
+# print("Elevator B Scenario 3 Final Position ", scenario3.elevatorsList[1].currentFloor, "\n")
