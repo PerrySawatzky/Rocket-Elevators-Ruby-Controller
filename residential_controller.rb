30f19728da8e138193f4b2eecaea2787e25ba77b
PerrySawatzky <sawatzky.perry@gmail.com>
2021-06-02 12:01:19 -0400

    plays nice

--- residential_controller.rb
+++ residential_controller.rb
@@ -1,65 +1,65 @@
 #Create your column and the rest is taken care of.
 class Column
-    attr_accessor :ID, :status, :elevatorList, :callButtonList, :bestElevator, :bestFloorGap, :currentFloor
+    attr_accessor :ID, :status, :elevatorsList, :callButtonsList, :bestElevator, :bestFloorGap, :currentFloor
     def initialize(_id, _amountOfFloors, _amountOfElevators)
-        @@ID = _id
-        @@status = 'built'
-        @@elevatorList = []
-        @@callButtonList = []
+        @ID = _id
+        @status = 'built'
+        @elevatorsList = []
+        @callButtonsList = []
         #creates Elevators
-        for i in _amountOfElevators.step(1) do
+        (0..._amountOfElevators).each do |i|
             current_elevator = Elevator.new(i, _amountOfFloors)
-            elevatorList.push(current_elevator)
+            elevatorsList.push(current_elevator)
         end
         #creates CallButtons
-        for i in _amountOfFloors.step(1) do
+        (0..._amountOfFloors).each do |i|
             if(i != _amountOfFloors - 1)
                 newButtonUp = CallButton.new(i, i+1, 'up', 'off')
-                callButtonList.push(newButtonUp)
+                callButtonsList.push(newButtonUp)
             end
             if(i != 0)
                 newButtonDown = CallButton.new(i, i+1, 'down', 'off')
-                callButtonList.push(newButtonDown)
+                callButtonsList.push(newButtonDown)
             end
         end
     end
-    #Runs through the columns elevatorList and finds the smallest floor gap
+    #Runs through the columns elevatorsList and finds the smallest floor gap
     def requestElevator(_requestedFloor, _direction)
         bestElevator = nil
         bestFloorGap = 10000
 
-        #cycles through elevatorList and finds the bestFloorGap and assigns bestElevator
+        #cycles through elevatorsList and finds the bestFloorGap and assigns bestElevator
         #Also ensures the best elevator is idle
-        for i in elevatorList.length do
-            currentFloorGap = (elevatorList[i].currentFloor - _requestedFloor).abs
-            elevatorMovingStatus = elevatorList[i].status
+        (0...elevatorsList.size).each do |i|
+            currentFloorGap = (elevatorsList[i].currentFloor - _requestedFloor).abs
+            elevatorMovingStatus = elevatorsList[i].status
             if(elevatorMovingStatus != 'idle')
                 currentFloorGap = 10000
             end
             if(currentFloorGap < bestFloorGap)
                 bestFloorGap = currentFloorGap
-                bestElevator = elevatorList[i]
+                bestElevator = elevatorsList[i]
             end
         end
         #Moves elevator up or down until it matches _requestedFloor, then opens the door
         while(_requestedFloor < bestElevator.currentFloor)
+            bestElevator.currentFloor = bestElevator.currentFloor - 1
+            bestElevator.direction = 'down'
+            bestElevator.status = 'moving'
+            print("Elevator is on floor #{bestElevator.currentFloor}\n")
+        end
+        while(_requestedFloor > bestElevator.currentFloor)
             bestElevator.currentFloor = bestElevator.currentFloor + 1
             bestElevator.direction = 'up'
             bestElevator.status = 'moving'
-            print("Elevator is on floor #{bestElevator.currentFloor}")
-        end
-        while(_requestedFloor > bestElevator.currentFloor)
-            bestElevator.currentFloor = bestElevator - 1
-            bestElevator.direction = 'down'
-            bestElevator.status = 'moving'
-            print("Elevator is on floor #{bestElevator.currentFloor}")
+            print("Elevator is on floor #{bestElevator.currentFloor}\n")
         end
         while(_requestedFloor == bestElevator.currentFloor)
             bestElevator.direction = nil
             bestElevator.door.status = 'open'
             bestElevator.status = 'idle'
-            print("*DING* The elevator doors are #{bestElevator.door.status}")
-            print("Your elevator has arrived, please enter now.")
+            print("*DING* The elevator doors are #{bestElevator.door.status}\n")
+            print("Your elevator has arrived, please enter now.\n")
             break
         end
         return bestElevator #So we can call on the function and give us the same elevator when using requestFloor when testing
@@ -67,39 +67,39 @@ class Column
 end
 #Automatically created when Column is created
 class Elevator
-    attr_accessor :ID, :status, :direction, :currentFloor, :door, :floorRequestButtonList, :floorRequestList
+    attr_accessor :ID, :status, :direction, :currentFloor, :door, :floorRequestButtonsList, :floorRequestList
     def initialize(_id, _amountOfFloors)
-        @@ID = _id
-        @@status = 'idle'
-        @@direction = nil
-        @@currentFloor = 1
-        @@door = Door.new(_id)
-        @@floorRequestButtonList = []
-        @@floorRequestList = []
+        @ID = _id
+        @status = 'idle'
+        @direction = nil
+        @currentFloor = 1
+        @door = Door.new(_id)
+        @floorRequestButtonsList = []
+        @floorRequestList = []
         #Creates 1 FloorRequestButtons for each floor
-        for i in _amountOfFloors.step(1) do
+        (0..._amountOfFloors).each do |i|
             newFloorRequestButton = FloorRequestButton.new(i-1, i)
-            floorRequestButtonList.push(newFloorRequestButton)
+            floorRequestButtonsList.push(newFloorRequestButton)
         end
     end
-    #Same idea as the requestElevator method, moves up or down one floor at a time until requestedFloor is equal to currentFloor
+    # Same idea as the requestElevator method, moves up or down one floor at a time until requestedFloor is equal to currentFloor
     def requestFloor(_requestedFloor)
-        while currentFloor > _requestedFloor
-            currentFloor = currentFloor - 1
+        while self.currentFloor > _requestedFloor
+            self.currentFloor = self.currentFloor - 1
             door.status = 'closed'
             status = 'moving'
-            print("Elevator is on floor #{currentFloor}")
+            print("Elevator is on floor #{self.currentFloor}\n")
         end
-        while currentFloor < _requestedFloor
-            currentFloor = currentFloor + 1
+        while self.currentFloor < _requestedFloor
+            self.currentFloor = self.currentFloor + 1
             door.status = 'closed'
             status = 'moving'
-            print("Elevator is on floor #{currentFloor}")
+            print("Elevator is on floor #{currentFloor}\n")
         end
-        while currentFloor == _requestedFloor
+        while self.currentFloor == _requestedFloor
             door.status = 'open'
             status = 'idle'
-            print('Your elevator has arrived, please exit now.')
+            print("Your elevator has arrived, please exit now.\n")
             break
         end
     end
@@ -108,12 +108,12 @@ end
 
 #Automatically created when Column is created
 class CallButton
-    attr_accessor :ID, :floor, :direction, :light,
+    attr_accessor :ID, :floor, :direction, :light
     def initialize(_id, _floor, _direction, _light)
-        @@ID = _id
-        @@floor = _floor
-        @@direction = _direction
-        @@light = _light
+        @ID = _id
+        @floor = _floor
+        @direction = _direction
+        @light = _light
     end
 end
 
@@ -121,9 +121,9 @@ end
 class FloorRequestButton
     attr_accessor :ID, :status, :floor
     def initialize(_id, _floor)
-        @@ID = _id
-        @@status = 'off'
-        @@floor = _floor
+        @ID = _id
+        @status = 'off'
+        @floor = _floor
     end
 end
 
@@ -131,16 +131,15 @@ end
 class Door
     attr_accessor :ID, :status
     def initialize(_id)
-        @@ID = _id
-        @@status = 'closed'
+        @ID = _id
+        @status = 'closed'
     end
 end
 
-# testColumn = Column.new(1, 10, 2)
-# testColumn.elevatorList[0].currentFloor = 2
-# testColumn.elevatorList[1].currentFloor = 6
-# elevatorToUse = testColumn.requestElevator(3, 'up')
-# elevatorToUse.requestFloor(7)
-# print("Elevator A Scenario 1 Final Position", testColumn.elevatorList[0].currentFloor)
-# print("Elevator B Scenario 1 Final Position", testColumn.elevatorList[1].currentFloor)
-# print("I can't believe this worked.")
+testColumn = Column.new(1, 10, 2)
+testColumn.elevatorsList[0].currentFloor = 2
+testColumn.elevatorsList[1].currentFloor = 6
+elevatorToUse = testColumn.requestElevator(3, 'up')
+elevatorToUse.requestFloor(7)
+print("Elevator A Scenario 1 Final Position ", testColumn.elevatorsList[0].currentFloor, "\n")
+print("Elevator B Scenario 1 Final Position ", testColumn.elevatorsList[1].currentFloor, "\n")
